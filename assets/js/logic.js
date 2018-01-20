d6a9c61a94cbf5eed35f0c97343373bd001fa354
derek-haworth <derekhaw1@gmail.com>
2018-01-20 12:11:39 -0600

    add google map api

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAJCyPmkk2azsrA9HMVCBCRH9g4dO70H9o",
+  authDomain: "project-one-maps.firebaseapp.com",
+  databaseURL: "https://project-one-maps.firebaseio.com",
+  projectId: "project-one-maps",
+  storageBucket: "project-one-maps.appspot.com",
+  messagingSenderId: "735214191875"
+};
+
+firebase.initializeApp(config);
+
