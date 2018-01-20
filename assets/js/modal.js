faf10bc7960b67078293bf5e81e99be203c21201
Enas Siddiqi <enassiddiqi@dhcp-10-102-233-126.wireless.northwestern.private>
2018-01-20 12:48:39 -0600

    Start data structure for Firebase

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,65 +1,103 @@
 $(document).ready(function(){
-// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-$('#modal1').modal();
+	// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+	$('#modal1').modal();
 
-console.log("MODAL");
-});
 
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-});
 
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBJVVmkoI4kqEULZglV7X0oUfeT2_LuReA",
-	authDomain: "project-1-8d0e8.firebaseapp.com",
-	databaseURL: "https://project-1-8d0e8.firebaseio.com",
-	projectId: "project-1-8d0e8",
-	storageBucket: "project-1-8d0e8.appspot.com",
-	messagingSenderId: "1010869223986"
-};
+	// Initialize Firebase
 
-firebase.initializeApp(config);
+	var config = {
+		apiKey: "AIzaSyCA3PWKYA42bM2Tz7ofOY72jOI2QdtH5gM",
+		authDomain: "apartmentyelp.firebaseapp.com",
+		databaseURL: "https://apartmentyelp.firebaseio.com",
+		projectId: "apartmentyelp",
+		storageBucket: "apartmentyelp.appspot.com",
+		messagingSenderId: "339519487350"
+	};
 
-// Create a variable to reference the database.
-var database = firebase.database();
+	firebase.initializeApp(config);
 
-// Initial Values
-var bldgCondition = 0;
-var water = 0;
-var airQuality = 0;
-var management = 0;
-var pests = 0;
-var electricity = 0;
-var hiddenFees = 0;
-var cellReception = 0;
+	// Create a variable to reference the database.
+	var database = firebase.database();
 
-// Capture Button Click
-$(".modal-submit").on("click", function(event) {
 
-	event.preventDefault();
+	// Initial Values
+	var count = 0;
+	var newReview = { };
+	var name = "";
+	var date = ""; // Date
+	var unit = "";
+	var leaseDur = 0;
 
-	// Grabbed values from text-boxes
-	name = $("#name-input").val().trim();
-	email = $("#email-input").val().trim();
-	age = $("#age-input").val().trim();
-	comment = $("#comment-input").val().trim();
+	// Star Ratings
+	var bldgCondition = 0;
+	var water = 0;
+	var tempReg = 0; // new
+	var airQuality = 0;
+	var management = 0;
+	var pests = 0;
+	var electricity = 0;
+	var hiddenFees = 0;
+	var cellRecept = 0;
+	var internet = 0; // new
 
-	// Code for "Setting values in the database"
-	database.ref().set({
-		buildingCondition: bldgCondition,
-		water: water,
-		airQuality: airQuality,
-		management: management,
-		pests: pests,
-		electricity: electricity,
-		hiddenFees: hiddenFees,
-		cellReception: cellReception
+	// Comments
+	var comments = "";
+
+	// Make object helper function
+	var makeObj = function () {
+		newReview = {
+			name: name,
+			date: date,
+			unit: unit,
+			leaseDur: leaseDur,
+			// star reviews:
+			bldgCondition: bldgCondition,
+			water: water,
+			tempReg: tempReg,
+			airQuality: airQuality,
+			management: management,
+			pests: pests,
+			electricity: electricity,
+			hiddenFees: hiddenFees,
+			cellReception: cellRecept,
+			internet: internet,
+			// additional comments:
+			comments: comments
+		};
+	};
+
+
+
+
+
+
+
+	// Capture Button Click
+	$(".modal-submit").on("click", function(event) {
+		console.log("MODAL");
+
+		event.preventDefault();
+
+		// Grabbed values from form
+		name = $("#name").val().trim();
+		unit = $("#unit").val().trim();
+		leaseDur = $("#lease-duration").val();
+		comments = $("#comments").val().trim();
+
+
+		// Code for "Setting values in the database"
+		database.ref().set({
+			count: count
+
+		});
+
 	});
 
+
 });
+
+
+
+
+
