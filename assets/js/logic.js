f03e57977cb10547888dcdf68a101a7f9d59700d
derek-haworth <derekhaw1@gmail.com>
2018-01-20 16:04:58 -0600

    add index.html and pseudo code the google api flow

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,19 @@
+/*
+1. Establish geolocation on page load
+2. if user declines then set coordinates to Chicago Loop
+3. Implement autocomplete address in search bar
+4. Search address and upon click or enter then initialize Google Map Api
+5. Parse address into Lat, Long coordinates
+6. Set marker and info window conditions
+7. Push coordinates into Firebase
+8. If user clicks review, trigger modal window
+8a. If user cancels or clicks view then clear out coordinates in Firebase.
+9. 
+
+
+*/
+
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyAJCyPmkk2azsrA9HMVCBCRH9g4dO70H9o",
@@ -9,4 +25,97 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
+
+$(document).ready(function(){
+	// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+	$('#modal1').modal();
+
+
+
+	// Initialize Firebase
+
+	var config = {
+		apiKey: "AIzaSyCA3PWKYA42bM2Tz7ofOY72jOI2QdtH5gM",
+		authDomain: "apartmentyelp.firebaseapp.com",
+		databaseURL: "https://apartmentyelp.firebaseio.com",
+		projectId: "apartmentyelp",
+		storageBucket: "apartmentyelp.appspot.com",
+		messagingSenderId: "339519487350"
+	};
+
+	firebase.initializeApp(config);
+
+	// Create a variable to reference the database.
+	var database = firebase.database();
+
+
+	// Initial Values
+	var newReview = {},
+		name = "",
+		date = "", // Date
+		unit = "",
+		leaseDur = 0;
+
+	// Star Ratings
+	var bldgCondition = 0,
+		water = 0,
+		tempReg = 0, // new
+		airQuality = 0,
+		management = 0,
+		pests = 0,
+		electricity = 0,
+		hiddenFees = 0,
+		cellRecept = 0,
+		internet = 0; // new
+	// Comments
+	var comments = "";
+
+
+	// Capture Button Click
+	$(".modal-submit").on("click", function(event) {
+		console.log("Submit Modal data");
+
+		event.preventDefault();
+
+		// Grabbed values from form
+		name = $("#name").val().trim();
+		unit = $("#unit").val().trim();
+		leaseDur = $("#lease-duration").val();
+		comments = $("#comments").val().trim();
+
+		var apartments = {
+				name: name,
+				date: date,
+				unit: unit,
+				leaseDur: leaseDur,
+				bldgCondition: bldgCondition,
+				water: water,
+				tempReg: tempReg,
+				airQuality: airQuality,
+				management: management,
+				pests: pests,
+				electricity: electricity,
+				hiddenFees: hiddenFees,
+				cellReception: cellRecept,
+				internet: internet,
+				// additional comments:
+				comments: comments
+			}
+		};
+
+
+		// Code for "Setting values in the database"
+		database.ref().push(apartments);
+
+	});
+
+
+});
+
+
+
+
+
+
 
