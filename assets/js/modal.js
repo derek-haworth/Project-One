8df64b9eb491b1623c2f57751a4f7db0e7aee89a
Caitlin Everett <pwt783@mac1s-MacBook-Pro-68.local>
2018-01-22 19:47:05 -0600

    updated modal.js with initial firebase stuff

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,103 +2,49 @@ $(document).ready(function(){
 	// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 	$('#modal1').modal();
 
-
 	// Initialize Firebase
-
 	var config = {
-		apiKey: "AIzaSyBJVVmkoI4kqEULZglV7X0oUfeT2_LuReA",
-		authDomain: "project-1-8d0e8.firebaseapp.com",
-		databaseURL: "https://project-1-8d0e8.firebaseio.com",
-		projectId: "project-1-8d0e8",
-		storageBucket: "project-1-8d0e8.appspot.com",
-		messagingSenderId: "1010869223986"
+		apiKey: "AIzaSyCA3PWKYA42bM2Tz7ofOY72jOI2QdtH5gM",
+		authDomain: "apartmentyelp.firebaseapp.com",
+		databaseURL: "https://apartmentyelp.firebaseio.com",
+		projectId: "apartmentyelp",
+		storageBucket: "apartmentyelp.appspot.com",
+		messagingSenderId: "339519487350"
 	};
 
-	var app = firebase.initializeApp(config);
+	firebase.initializeApp(config);
 
 	// Create a variable to reference the database.
-	var database = app.database();
+	var database = firebase.database();
+	var apartments = firebase.database().ref('apartments');
 
 
-	// // Initial Values
-	// var count = 0;
-	// var newReview = { };
-	// var name = "";
-	// var date = ""; // Date
-	// var unit = "";
-	// var leaseDur = 0;
+	//latitude-longitude
+	var latLong = "41^8781_87^6299";  //hard coded for now
 
-	// // Star Ratings
-	// var bldgCondition = 0;
-	// var water = 0;
-	// var tempReg = 0; // new
-	// var airQuality = 0;
-	// var management = 0;
-	// var pests = 0;
-	// var electricity = 0;
-	// var hiddenFees = 0;
-	// var cellRecept = 0;
-	// var internet = 0; // new
+	// Initial Values
+	var count = 0;
+	var newReview = { };
+	var name = "";
+	var date = ""; // Date
+	var unit = "";
+	var leaseDur = 0;
+	
 
-	// // Comments
-	// var comments = "";
+	// Star Ratings
+	var bldgCondition = 0;
+	var water = 0;
+	var tempReg = 0; // new
+	var airQuality = 0;
+	var management = 0;
+	var pests = 0;
+	var electricity = 0;
+	var hiddenFees = 0;
+	var cellRecept = 0;
+	var internet = 0; // new
 
-	// // Make object helper function
-	// var makeObj = function () {
-	// 	newReview = {
-	// 		name: name,
-	// 		date: date,
-	// 		unit: unit,
-	// 		leaseDur: leaseDur,
-	// 		// star reviews:
-	// 		bldgCondition: bldgCondition,
-	// 		water: water,
-	// 		tempReg: tempReg,
-	// 		airQuality: airQuality,
-	// 		management: management,
-	// 		pests: pests,
-	// 		electricity: electricity,
-	// 		hiddenFees: hiddenFees,
-	// 		cellReception: cellRecept,
-	// 		internet: internet,
-	// 		// additional comments:
-	// 		comments: comments
-	// 	};
-	// };
-
-	// Vue.component('star-rating', VueStarRating.default)
-
-	// new Vue({
-	// 	el: '#app',
-
-	// 	data: {
-	// 		bldgCondition: 0,
-	// 		water: 0,
-	// 		air: 0,
-	// 		management: 0,
-	// 		pests: 0,
-	// 		electricity: 0,
-	// 		hiddenFees: 0,
-	// 		cellReception: 0,
-	// 		tempReg: 0,
-	// 		internet: 0
-	// 	},
-
-	// 	methods: {
-	// 		submitReview: function() {
-	// 			var data = new FormData
-	// 			this.$http.post('https://project-1-8d0e8.firebaseio.com/', { bldgCondition: this.bldgCondition, water: this.water }).then(
-	// 				function(response) {
-	// 					window.location.href="https://project-1-8d0e8.firebaseio.com/" ;
-	// 				},
-	// 				function(response) {
-	// 					that.errors = response.data.errors
-	// 				}
-	// 				)
-	// 		}
-	// 	}
-	// });	
-
+	// Comments
+	var comments = "";
 
 
 	// Capture Button Click
@@ -113,28 +59,71 @@ $(document).ready(function(){
 		leaseDur = $("#lease-duration").val();
 		comments = $("#comments").val().trim();
 
+		
+		var review = {
+				name: name,
+				date: date,
+				unit: unit,
+				leaseDur: leaseDur,
+				// star reviews:
+				bldgCondition: bldgCondition,
+				water: water,
+				tempReg: tempReg,
+				airQuality: airQuality,
+				management: management,
+				pests: pests,
+				electricity: electricity,
+				hiddenFees: hiddenFees,
+				cellReception: cellRecept,
+				internet: internet,
+				// additional comments:
+				comments: comments
+			};
+
+			var summary = {
+				"stuff" : "stuff", //more coming soon
+			}
+
+			var count = 4;
+
 
 		// Code for "Setting values in the database"
-		database.ref().push({
-			name: name,
-			unit: unit,
-			leaseDur: leaseDur,
-			comments: comments
-		});
-
+		// database.ref('/data.name').push(apartments);
+		firebase.database().ref([latLong]+ "/reviews").push(review);
+		firebase.database().ref([latLong]+ "/summary").set(summary);
+		firebase.database().ref([latLong]+ "/count").set(count);
 	});
-
 });
 
+//TODO have a function that runs whenever I add a review to my firebase (child added I believe)
 
+//In that function first iterate through the the reviews "object" using a for in loop
+//In that for in loop add sum.reviewProperty of each review
+var sum = 0;
+/*for(review in reviews) {
+sum.bldgCondition+=reviews[review].bldgCondition;
+sum.water+=review.water
+sum.tempReg+=review.tempReg
+sum.airQuality+=review.airQuality
+sum.management+=review.management
++ other things
+}
+calculate length of object by running Object.keys(whatever the object is)
+for(sum in summmary) {
+property.avg = sum / count;
+}
 
-// var rating = $("#app > div:nth-child(1) > div.bldgCondition-rating.vue-star-rating > div > span.vue-star-rating-rating-text").text().trim();
+var obj = {
+'1': '456',
+'2': 'Whatever',
+'3': "who cares"
+}
 
-// var captureRating = function(ratingClass) {
-// 	$(ratingClass).on("click"), function(){
-
-// 	}
-// }
-
-
-
+for(o in obj) {
+//console.log(o)
+console.log(obj[o])
+}
+outputs :
+"456"
+"Whatever"
+"who cares" */
